aca7c7e30bf90b0672021730f39be1ee7d5dc9f4
rehmanshawon <meghnadbadh@gmail.com>
2022-09-03 17:31:17 +0600

    class 01

---
+++ test.js
@@ -0,0 +1,38 @@
+var n1 = 50;
+var n2 = 25;
+
+console.log("n1+n2 = ", n1 + n2);
+
+345 decimal to binary
+
+345 / 2=172
+1
+172 / 2=86
+0
+86 / 2=43
+0
+43 / 2=21
+1
+21 / 2=10
+1
+10 / 2=5
+0
+5 / 2=2
+1
+2 / 2=1
+0
+1 / 2=0
+1
+
+subtract binary numbers using 1's compliment
+110101 - 100010
+100010 = 011101 // complement of sutrahend
+110101 + 011101 // add with minuend
+
+    110101 
+    011101
+    -------
++1  010010  // 1 carry over
+         1
+-------------
+     10011 result     
