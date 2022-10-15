ca2f89c16307daadd6659e2d354380c17376fa96
rehmanshawon <meghnadbadh@gmail.com>
2022-10-15 18:56:22 +0600

    variables

--- variables.js
+++ variables.js
@@ -1,4 +1,4 @@
-let username = "shouvik";
+let username = "shouvik"; //string type
 username = "aa";
 
 console.log(username);
@@ -7,3 +7,45 @@ const number = 100;
 for (let i = 1; i <= number; i *= 2) {
   console.log(i);
 }
+
+// string variables
+
+let user = "shawon";
+let password = "123abc";
+
+// number variables
+let pi = 3.14;
+let age = 25;
+
+// boolean
+let loggedIn = true;
+let sessionExpired = false;
+
+// objecs
+let teamScores = {
+  Rajshahi: 78,
+  Dhaka: 85,
+  Barishal: 59,
+};
+
+// Arrays
+let marks = [55, 74, 65, 90]; // number arrays
+let userIds = ["i01", "i02", "i03"]; //string arrays
+// array of Objects
+let leagueResults = [
+  {
+    Rajshahi: 78,
+    Dhaka: 85,
+    Barishal: 59,
+  },
+  {
+    Rajshahi: 65,
+    Dhaka: 90,
+    Barishal: 75,
+  },
+  {
+    Rajshahi: 51,
+    Dhaka: 96,
+    Barishal: 77,
+  },
+];
